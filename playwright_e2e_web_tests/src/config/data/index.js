b7ccce2edff941c7205cdb289446ebc647967634
Leonardo Luiz Antunes <leoluizantunes@gmail.com>
2023-01-04 12:30:32 -0300

    feat: add playwright configuration

---
+++ playwright_e2e_web_tests/src/config/data/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    login: require('./hml/login.data'),
+    inventory: require('./hml/inventory.data'),
+    checkout: require('./hml/checkout.data')
+}
---
+++ playwright_e2e_web_tests/tests/index.spec.js
@@ -0,0 +1,3 @@
+// LOG test config
+console.log('Test Configuration:')
+console.log({ TARGET, CI, ...CONFIG_ENV })
